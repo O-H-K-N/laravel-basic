1265fa32e472c155ca37fe895c0179dbefc7b017
大塚健登 <kent0tsuka1118@gmail.com>
2022-10-31 23:54:59 +0900

    Fix：ルーティング、コントローラ、ビューに設定を追加し、ブラウザで表示させてみる

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,7 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+// モデルの読み込み
+use App\Models\Test;
+
 class TestController extends Controller
 {
-    //
+    // indexアクションを設定
+    public function index()
+    {
+        // 全件取得
+        $values = Test::all();
+
+        // デバッガー：処理を止め内容を確認できる
+        // dd($values);
+
+        // compact関数でView側に変数を渡すと楽
+        return view('tests.test', compact('values'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// コントローラを読み込み
+use App\Http\Controllers\TestController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ルートを作成
+Route::get('tests/test', [TestController::class, 'index'] );
