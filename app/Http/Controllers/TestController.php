b227b57c1222acadefe093a5014b1ca47ef2c9ba
大塚健登 <kent0tsuka1118@gmail.com>
2022-11-01 15:21:56 +0900

    Add：コレクション型を使った処理の確認

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -12,11 +12,21 @@ class TestController extends Controller
     // indexアクションを設定
     public function index()
     {
+        /* Eloquent(エロクアント) */
         // 全件取得
         $values = Test::all();
 
+        // DB内のデータ数を取得
+        $count = Test::count();
+
+        // 指定したidのデータを取得
+        $first = Test::findOrFail(1);
+
+        // 条件指定したデータを取得
+        $whereBBB = Test::where('text', '=', 'bbb')->get();
+
         // デバッガー：処理を止め内容を確認できる
-        // dd($values);
+        dd($values, $count, $first, $whereBBB);
 
         // compact関数でView側に変数を渡すと楽
         return view('tests.test', compact('values'));
